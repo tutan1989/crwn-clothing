f95c71b99c59395e02432d57881f3a3400c47b5b
Tutan Das <tutan.essential@gmail.com>
2020-01-15 21:27:05 +0530

    initial commit and home page added

---
+++ src/homepage.comopent.jsx
@@ -0,0 +1,42 @@
+import  React from 'react';
+import './homepage.styles.scss';
+
+const HopePage = () =>(
+
+    <div className="hopepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">hats</h1>
+                    <span className="subtitle">show now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">jackets</h1>
+                    <span className="subtitle">show now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">showes</h1>
+                    <span className="subtitle">show now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">women</h1>
+                    <span className="subtitle">show now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Men</h1>
+                    <span className="subtitle">show now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HopePage;
