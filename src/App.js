698655310c577cab080d6f128f849c0ceb97494c
Tutan Das <tutan.essential@gmail.com>
2020-01-23 11:27:57 +0530

    shop page and its component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import HopePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.component.jsx';
+import {Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <HopePage></HopePage>
+      <Route exact path="/" component={HopePage} />
+      <Route path="/shop" component={ShopPage} />  
     </div>
   );
 }
