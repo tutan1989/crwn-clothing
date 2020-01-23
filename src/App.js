1133598672d713cb6909dc7f0a164ddbc96a8ca4
Tutan Das <tutan.essential@gmail.com>
2020-01-23 15:40:02 +0530

    Header added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import HopePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
-import {Route} from 'react-router-dom';
+import Header from './components/header/header.component.jsx';
+import {Route, Switch} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
+      <Switch>
       <Route exact path="/" component={HopePage} />
-      <Route path="/shop" component={ShopPage} />  
+      <Route path="/shop" component={ShopPage} /> 
+      </Switch> 
     </div>
   );
 }
